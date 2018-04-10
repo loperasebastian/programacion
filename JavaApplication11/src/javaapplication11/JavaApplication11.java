d6eb346012ef2273b113bea6f319a480c84069bd
loperasebastian <38219737+loperasebastian@users.noreply.github.com>
2018-04-09 23:28:15 -0500

    modifique el comentario

--- JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -17,7 +17,7 @@ public class JavaApplication11 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo primera modificacion");
     }
     
 }
